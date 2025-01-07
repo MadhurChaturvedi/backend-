1f914b5286669d53cc34b3717907041bc594a50e
Your Name <madhurchaturvedi2000@gmail.com>
2025-01-07 21:29:47 +0530

    create basic server

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
-
+require('dotenv').config()
 const app =  express();
 app.use(express.json())
 
 
-app.listen(8080,()=>{
-    console.log(`Server is running on port :${8080}`);
+const PORT=process.env.PORT
+app.listen(PORT,()=>{
+    console.log(`Server is running on port https//localhost:${PORT} 🤖👾👺`);
 })
