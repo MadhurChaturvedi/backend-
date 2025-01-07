fc42c94477ed37c106ad6db2844db2cb6c570267
Your Name <madhurchaturvedi2000@gmail.com>
2025-01-07 22:01:53 +0530

    add push

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-const express = require("express");
-require('dotenv').config()
-const app =  express();
-app.use(express.json())
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+import connectDB from "./config/db.js";
+
+const app = express();
+app.use(express.json());
+// connect the DB
+connectDB();
 
 
-const PORT=process.env.PORT
+const PORT=process.env.PORT || 5000
 app.listen(PORT,()=>{
     console.log(`Server is running on port https//localhost:${PORT} 🤖👾👺`);
 })
