3687e68b52a43d21d1e47b06870e4b7d30d50525
Your Name <madhurchaturvedi2000@gmail.com>
2025-01-07 22:01:38 +0530

    add db connection

---
+++ config/db.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+    try {
+        const connection = await mongoose.connect(process.env.DB, {
+            dbName: "backend_star",
+        });
+        console.log(`MongoDB Connected: ${connection.connection.host}`);
+    } catch (error) {
+        console.error(`Error: ${error.message}`);
+        process.exit(1); // Exit process with failure
+    }
+};
+
+export default connectDB;
