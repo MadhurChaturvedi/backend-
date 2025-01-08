f8d065b9bd34bd2e043ffb8d9273e7c27d0756c5
Your Name <madhurchaturvedi2000@gmail.com>
2025-01-08 19:59:24 +0530

    add console color dependency

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import connectDB from "./config/db.js";
+import colors from 'colors';
 
 const app = express();
 app.use(express.json());
@@ -11,5 +12,5 @@ connectDB();
 
 const PORT=process.env.PORT || 5000
 app.listen(PORT,()=>{
-    console.log(`Server is running on port https//localhost:${PORT} 🤖👾👺`);
+    console.log(`Server is running on port https//localhost:${PORT} 🤖👾👺`.green);
 })
