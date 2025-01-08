b29115321ee70255b30c93226903777fea80193c
Your Name <madhurchaturvedi2000@gmail.com>
2025-01-09 00:34:19 +0530

    user route is ready

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,4 @@
-import { User } from "../model/user";
+import { User } from "../model/user.js";
 import bcrypt from "bcryptjs";
 
 export const register = async (req, res) => {
@@ -20,20 +20,22 @@ export const register = async (req, res) => {
             }
 
             const hashedPassword = await bcrypt.hash(password,10);
-
             const newUser = await new User({
                 fullName,
                 email,
                 password:hashedPassword
             })
 
-            await newUser.save();
+             await newUser.save();
             res.status(200).json({
                 success: true,
                 message: "Account created Successfully."
             })
         }
     } catch (error) {
-
+        res.status(404).json({
+            success: true,
+            message: "Somthing went wrong server error"
+        })
     }
 }
--- index.js
+++ index.js
@@ -3,14 +3,19 @@ import dotenv from "dotenv";
 dotenv.config();
 import connectDB from "./config/db.js";
 import colors from 'colors';
+import userRouter from "./routes/user.js"
 
 const app = express();
 app.use(express.json());
 // connect the DB
 connectDB();
 
+app.use("/api/user",userRouter);
 
+app.get("/",(req,res)=>{
+    res.send("NODE ❤️")
+})
 const PORT=process.env.PORT || 5000
 app.listen(PORT,()=>{
-    console.log(`Server is running on port https//localhost:${PORT} 🤖👾👺`.green);
+    console.log(`Server is running on port https://localhost:${PORT} 🤖👾👺`.green);
 })
