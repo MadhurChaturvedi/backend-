4924df8751b089051f17f44618f9b211a9c16fff
Your Name <madhurchaturvedi2000@gmail.com>
2025-01-09 14:17:25 +0530

    add bodyparser

--- index.js
+++ index.js
@@ -3,18 +3,21 @@ import dotenv from "dotenv";
 dotenv.config();
 import connectDB from "./config/db.js";
 import colors from 'colors';
+import bodyParser from "body-parser";
 import userRouter from "./routes/user.js"
 
 const app = express();
-app.use(express.json());
 // connect the DB
 connectDB();
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.use("/api/v1/user",userRouter);
 
 app.get("/",(req,res)=>{
     res.send("NODE ❤️")
 })
+
 const PORT=process.env.PORT || 5000
 app.listen(PORT,()=>{
     console.log(`Server is running on port http://localhost:${PORT} 🤖👾👺`.green);
